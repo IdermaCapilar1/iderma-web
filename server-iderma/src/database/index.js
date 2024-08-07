5cd5d3de59226d3351a5e7ab0c8dea65cf038c8c
IdermaCapilar <idermadesarrollorweb2@gmail.com>
2024-08-07 17:37:36 -0500

    fix

--- server-iderma/src/database/index.js
+++ server-iderma/src/database/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import mysql from 'mysql'
+import mysql from 'mysql2'
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
 const connection = mysql.createConnection({
