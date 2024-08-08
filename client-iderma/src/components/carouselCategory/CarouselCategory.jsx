42528c56eb7050815543fc1272d83a300228a4e9
IdermaCapilar <idermadesarrollorweb2@gmail.com>
2024-08-08 18:01:10 -0500

    estilos

---
+++ client-iderma/src/components/carouselCategory/CarouselCategory.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function CarouselCategory() {
+  return (
+    <div className="m-5 flex w-full">
+      {/* title carousel */}
+      <div>
+        <h3 className="font-gatchye text-blue-800">
+          Acerca de nuestros servicios
+        </h3>
+        <h1>Brindamos experiencia única para ti</h1>
+      </div>
+    </div>
+  );
+}
+
+export default CarouselCategory;
